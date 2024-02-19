4f3f81ecb934261537f48a22512580208a4ac6c6
Aorui Tan <aorui.tan@Aoruis-MacBook-Pro.local>
2024-02-19 14:38:00 +1100

    build shopping item form

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import ShoppingList from "./components/ShoppingList";
+import Item from "./models/Item";
+import ShoppingItemForm from "./components/ShoppingItemForm";
+
+function App() {
+  // const items = [
+  //   { id: 1, product: "apple", quantity: 2 },
+  //   { id: 2, product: "pear", quantity: 5 },
+  //   { id: 3, product: "orange", quantity: 3 },
+  // ];
+
+  const [items, setItems] = useState<Item[]>([]);
+
+  return (
+    <>
+      {/* <Greeter person="Tom" /> */}
+      <ShoppingList items={items} />
+      <ShoppingItemForm setItems={setItems} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ShoppingItemForm.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Item from "../models/Item";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+function ShoppingItemForm({
+  setItems,
+}: {
+  setItems: React.Dispatch<React.SetStateAction<Item[]>>;
+}) {
+  const [product, setProduct] = useState<string>("");
+  const [quantity, setQuantity] = useState<number>(0);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    setItems((items) => {
+      return [
+        ...items,
+        { id: items.length + 1, product: product, quantity: quantity },
+      ];
+    });
+    setProduct("");
+    setQuantity(0);
+  }
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Product Name"
+        value={product}
+        onChange={(e) => setProduct(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Quantity"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      />
+      <button type="submit">Add Item</button>
+    </StyledForm>
+  );
+}
+
+export default ShoppingItemForm;
